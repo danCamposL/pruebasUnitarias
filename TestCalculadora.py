6205022a14cd6d20a0d5e5f8b6c503718289479b
danielcampos586@gmail.com <danielcampos586@gmail.com>
2015-09-11 23:56:24 +0800

    Agregue prueba de calculadora

---
+++ TestCalculadora.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestCalculadora(unittest.TestCase):
+	def test_suma_de_2_mas_2(self):
+		calc = Calculadora()
+		
+		resultado = calc.suma(2,2)
+		
+		self.assertEqual
+		
+		
